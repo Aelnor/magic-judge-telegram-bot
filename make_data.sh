3dc417a54b1642074018f2912c44bdc1ef19d0f9
Alexey Chernyshov <eiden127@gmail.com>
2019-07-23 18:00:05 +0000

    Remove CR codepage conversion

--- make_data.sh
+++ make_data.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 mkdir -p data
 python3 scripts/update_cards.py
-curl http://media.wizards.com/2018/downloads/MagicCompRules%2020181005.txt | iconv -f cp1251 -t utf8 > cr.txt
+curl https://media.wizards.com/2019/downloads/MagicCompRules%2020190503.txt --output cr.txt
 python3 scripts/update_cr.py
