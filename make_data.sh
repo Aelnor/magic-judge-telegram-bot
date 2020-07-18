36fcb06c7286399740bba223c916f511d7474c6f
Alexey Chernyshov <eiden127@gmail.com>
2020-07-18 14:13:18 +0000

    update make_data for the latest rules

--- make_data.sh
+++ make_data.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 mkdir -p data
 python3 scripts/update_cards.py
-curl https://media.wizards.com/2019/downloads/MagicCompRules%2020190503.txt --output cr.txt
+curl https://media.wizards.com/2020/downloads/MagicCompRules%2020200703.txt --output cr.txt
 python3 scripts/update_cr.py
+
